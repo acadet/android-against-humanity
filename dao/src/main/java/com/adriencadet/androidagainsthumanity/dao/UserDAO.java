142f4a5c10be63f519dafcecc0994e9f06555ee0
Adrien Cadet <acadet@live.fr>
2016-04-15 23:33:57 -0700

    Implements user DAO

---
+++ dao/src/main/java/com/adriencadet/androidagainsthumanity/dao/UserDAO.java
@@ -0,0 +1,27 @@
+package com.adriencadet.androidagainsthumanity.dao;
+
+import android.content.SharedPreferences;
+
+/**
+ * UserDAO
+ * <p>
+ */
+class UserDAO implements IUserDAO {
+    private static final String NICKNAME_KEY = "nickname";
+
+    private SharedPreferences sharedPreferences;
+
+    UserDAO(SharedPreferences sharedPreferences) {
+        this.sharedPreferences = sharedPreferences;
+    }
+
+    @Override
+    public String getNickname() {
+        return sharedPreferences.getString(NICKNAME_KEY, null);
+    }
+
+    @Override
+    public void saveNickname(String value) {
+        sharedPreferences.edit().putString(NICKNAME_KEY, value).commit();
+    }
+}
